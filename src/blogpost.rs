7c4448d77b2ac064e44ef41b11f23d394653345a
Fletch Rydell <fletch.rydell@duke.edu>
2023-08-26 00:28:50 -0400

    blog post markdown rendering w/ callbacks

--- src/blogpost.rs
+++ src/blogpost.rs
@@ -3,7 +3,7 @@ use std::{collections::HashMap, sync::Mutex};
 use chrono::NaiveDateTime;
 use color_eyre::eyre::eyre;
 use color_eyre::Result;
-use markdown::mdast::Node;
+use markdown::mdast::*;
 use once_cell::sync::Lazy;
 use serde::{Deserialize, Serialize};
 use tracing::error;
@@ -41,20 +41,107 @@ pub struct BlogPostContent {
 }
 
 impl BlogPostContent {
+    /// Render the content as markdown, but with a set of callbacks for handling some elements specially.
+    /// If multiple callbacks match an element, only the first is used.
+    pub fn render_markdown(&self, callbacks: &[RenderCallback]) -> Result<String> {
+        let mut rendered = String::new();
+        self.render_markdown_recurse(&self.markdown_ast, &mut rendered, callbacks)?;
+        Ok(rendered)
+    }
+    /// Recursive descent for markdown rendering. This is mainly used by `render_markdown`, but can be used
+    /// directly to render a subset of the markdown AST (for example, in a callback).
+    pub fn render_markdown_recurse(
+        &self,
+        node: &Node,
+        result: &mut String,
+        callbacks: &[RenderCallback],
+    ) -> Result<()> {
+        // Check for callbacks
+        for callback in callbacks {
+            if let Some(rendered) = callback.render(node, self, || {
+                let mut rendered = String::new();
+                for child in node.children().unwrap_or(&vec![]) {
+                    self.render_markdown_recurse(child, &mut rendered, callbacks)?;
+                }
+                Ok(rendered)
+            })? {
+                result.push_str(&rendered);
+                return Ok(());
+            }
+        }
+
+        // Helper macro to render children to result
+        macro_rules! children {
+            ($node:expr) => {
+                for child in $node.children.iter() {
+                    self.render_markdown_recurse(child, result, callbacks)?;
+                }
+            };
+        }
+
+        // Render node
+        match node {
+            Node::Root(r) => children!(r),
+            Node::FootnoteDefinition(d) => {
+                result.push_str(&format!("[^{}]: ", d.identifier));
+                children!(d);
+                result.push('\n');
+            }
+            Node::FootnoteReference(r) => result.push_str(&format!("[^{}]", r.identifier)),
+            Node::InlineCode(c) => result.push_str(&format!("`{}`", c.value)),
+            Node::Delete(d) => {
+                result.push_str("~~");
+                children!(d);
+                result.push_str("~~");
+            }
+            Node::Emphasis(e) => {
+                result.push_str("*");
+                children!(e);
+                result.push_str("*");
+            }
+            Node::Link(l) => {
+                result.push('[');
+                children!(l);
+                result.push_str(&format!("]({})", l.url));
+            }
+            Node::Strong(s) => {
+                result.push_str("**");
+                children!(s);
+                result.push_str("**");
+            }
+            Node::Text(t) => result.push_str(&t.value),
+            Node::Code(c) => result.push_str(&format!("```\n{}\n```", c.value)),
+            Node::Heading(h) => {
+                result.push_str(&"#".repeat(h.depth as usize));
+                result.push(' ');
+                children!(h);
+                result.push('\n');
+            }
+            Node::Paragraph(p) => {
+                children!(p);
+                result.push('\n');
+            }
+            _ => {
+                return Err(eyre!("Unhandled markdown node: {:?}", node));
+            }
+        }
+        Ok(())
+    }
+
     fn new(markdown: String) -> Result<Self> {
         // Parse AST
         let markdown_ast = markdown::to_mdast(&markdown, &markdown::ParseOptions::gfm())
             .map_err(|e| eyre!("{}", e))?;
 
-        // Find all code block children and do syntax highlighting
-        let all_descendants = all_descendants(&markdown_ast);
-        let code_blocks: Vec<_> = all_descendants
-            .iter()
-            .filter_map(|node| match node {
-                Node::Code(ref c) => Some(c),
-                _ => None,
-            })
-            .collect();
+        // Find all code block children with DFS
+        let mut code_blocks = vec![];
+        let mut to_visit = vec![&markdown_ast];
+        while let Some(node) = to_visit.pop() {
+            to_visit.extend(node.children().map(|v| v.iter()).unwrap_or_default());
+            if let Node::Code(ref c) = node {
+                code_blocks.push(c);
+            }
+        }
 
         // Generate code highlights map
         let mut code_highlights = HashMap::new();
@@ -128,14 +215,42 @@ pub struct HighlightedCode {
     pub content: Vec<(String, Option<usize>)>,
 }
 
-/// Utility to get all descendants of a markdown node.
-fn all_descendants(node: &Node) -> Vec<&Node> {
-    // Setup
-    let mut all_nodes = vec![node];
-    let mut to_visit = vec![node];
-    // Traversal
-    while let Some(node) = to_visit.pop() {
-        all_nodes.extend(node.children().map(|v| v.iter()).unwrap_or_default());
+/// A callback to alter the rendering of a specific node type.
+/// The callback is passed the node ref, the rendered content of the node's children, and any extra
+/// node-specific information (like syntax highlighting for code blocks).
+pub enum RenderCallback<'a> {
+    /// A callback for code blocks, with syntax highlighting info included.
+    Code(&'a dyn Fn(&Code, String, &HighlightedCode) -> String),
+    /// A callback for links.
+    Link(&'a dyn Fn(&Link, String) -> String),
+}
+impl<'a> RenderCallback<'a> {
+    /// Attempts to render a node with the callback, returning `None` if the callback doesn't handle the node.
+    /// The `content` is needed for extra information (like syntax highlighting), and the `rendered_children` is
+    /// used to get child content for the callback, if necessary.
+    fn render(
+        &self,
+        node: &Node,
+        content: &BlogPostContent,
+        rendered_children: impl Fn() -> Result<String>,
+    ) -> Result<Option<String>> {
+        Ok(match self {
+            RenderCallback::Code(f) => match node {
+                Node::Code(c) => Some(f(
+                    c,
+                    rendered_children()?,
+                    content
+                        .code_highlights
+                        .get(&c.position.as_ref().unwrap().start.offset)
+                        .ok_or_else(|| eyre!("Code block has no position info"))?,
+                )),
+
+                _ => None,
+            },
+            RenderCallback::Link(f) => match node {
+                Node::Link(l) => Some(f(l, rendered_children()?)),
+                _ => None,
+            },
+        })
     }
-    all_nodes
 }
